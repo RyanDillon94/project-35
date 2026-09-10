8d98fa927615af396846e2fde10a89bffb0e4b4e
RyanDillon94 <161262849+RyanDillon94@users.noreply.github.com>
2026-09-10 23:16:38 +0100

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -74,8 +74,8 @@ function Dashboard({ userId }: { userId: string }) {
 
       {/* Footer Management Section */}
       <div className="flex flex-col items-center gap-2 pt-4 border-t border-border/40">
-        <DeloadCard />
 <WeeklyTrendsAnalytics/>
+<DeloadCard />
         <DataBackupCard />
       </div>
 

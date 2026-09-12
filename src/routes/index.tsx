e0306135e613aa3d913d8fd366b0ea8052bd3e59
RyanDillon94 <161262849+RyanDillon94@users.noreply.github.com>
2026-09-12 23:44:22 +0100

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -49,8 +49,8 @@ function Dashboard({ userId }: { userId: string }) {
   return (
     <main className="mx-auto w-full max-w-xl space-y-4 px-4 pt-5 pb-28">
 
-      Hide test panel 
-      <TestModePanel /> 
+     {/* Hide test panel 
+      <TestModePanel /> */}
       
       
       <FinaliseWeekBanner userId={userId} />

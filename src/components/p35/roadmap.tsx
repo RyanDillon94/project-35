0038ba901925f69c6b3c1229966120f104151983
RyanDillon94 <161262849+RyanDillon94@users.noreply.github.com>
2026-09-15 18:49:35 +0100

    Update roadmap.tsx

--- src/components/p35/roadmap.tsx
+++ src/components/p35/roadmap.tsx
@@ -142,6 +142,11 @@ export function Roadmap() {
           </AccordionItem>
         ))}
       </Accordion>
+      <div className="flex flex-col items-center gap-2 pt-2">
+        <p className="text-center text-sm italic tracking-wide text-primary/90 font-medium">
+          &ldquo;Only cunts drink on weekdays... Don&apos;t be a cunt.&rdquo;
+        </p><br></br>
+      </div>
     </section>
   );
 }

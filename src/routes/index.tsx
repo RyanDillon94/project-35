f26acd1ea9292bad2f4182d9549077f428b51ca6
RyanDillon94 <161262849+RyanDillon94@users.noreply.github.com>
2026-09-19 20:23:49 +0100

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -99,8 +99,8 @@ function Dashboard({ userId }: { userId: string }) {
   return (
     <main className="mx-auto w-full max-w-xl space-y-4 px-4 pt-5 pb-28">
 
-       Hide test panel 
-      <TestModePanel /> 
+      {/* Hide test panel 
+      <TestModePanel /> */}
       
       {/* Top Banner (Only if NOT finalised) */}
       {!isFinalised && <FinaliseWeekBanner userId={userId} key={`top-${currentDate}`} />}

45848e637c0da265ba6ca3b89cc96cb105b2e238
RyanDillon94 <161262849+RyanDillon94@users.noreply.github.com>
2026-09-20 21:15:28 +0100

    Update finalise-week-banner.tsx

--- src/components/p35/finalise-week-banner.tsx
+++ src/components/p35/finalise-week-banner.tsx
@@ -1,4 +1,4 @@
-Import { useEffect, useState, useCallback } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { 
